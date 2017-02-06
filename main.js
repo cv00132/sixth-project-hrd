b27e01190ad3d41e2a716794cd5da79e77dba0c5
Cory Vaughan <codercory13@gmail.com>
2017-02-06 18:21:43 -0500

    Converted to hexadecimal.

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
 var display = document.getElementById("display");
 var bG = document.getElementById("bG");
 
+
 function clock() {
   var time = new Date();
-  var hours = time.getHours().toString();
-  var minutes = time.getMinutes().toString();
-  var seconds = time.getSeconds().toString();
-
-    if (hours > 12) {
-      hours = 0;
-    }
-   if (hours.length < 2) {
-     hours = "0" + hours;
-   }
-   if (minutes.length < 2) {
-     minutes = "0" + minutes;
-   }
-   if (seconds.length < 2) {
-     seconds = "0" + seconds;
-  }
+  var hours = time.getHours().toString(16);
+  var minutes = time.getMinutes().toString(16);
+  var seconds = time.getSeconds().toString(16);
 
 
-var timer = hours + ":" + minutes + ":" + seconds;
+var timer =  "#" + hours + minutes + seconds;
 var chameleonBG = "#" + hours + minutes + seconds;
 
 display.innerHTML = timer;
@@ -31,5 +19,6 @@ bG.style.backgroundColor = chameleonBG;
 setInterval(clock, 1000);
 
 console.log(clock());
+
 
 //setInterval(clock(), 1000);
